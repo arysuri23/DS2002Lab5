0d4366bcfc2f8b1b210cafaab24228abd8942c58
arysuri <59773116+arysuri23@users.noreply.github.com>
2022-11-13 16:47:04 -0500

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -1,9 +1,26 @@
-import requests, pgeocode
+import requests, pgeocode,  pymongo, pprint
+
 from bs4 import BeautifulSoup
 import pandas as pd
 
+#MONGODB VARIABLES
+host_name = "localhost"
+port = "27017"
+
+atlas_cluster_name = "sandbox"
+atlas_default_dbname = "local"
+
+conn_str = {
+    "local" : f"mongodb://{host_name}:{port}/",
+}
+
+client = pymongo.MongoClient(conn_str["local"])
+
+print(f"Local Connection String: {conn_str['local']}")
+
+
 #ask user for zipcode
-#zipcode = input("Please enter a U.S. postal code that you would like to get 7-day forecast for: ")
+zipcode = input("Please enter a U.S. postal code that you would like to get 7-day forecast for: ")
 
 nomi = pgeocode.Nominatim("us")
 location = nomi.query_postal_code("22102")
@@ -22,17 +39,17 @@ seven_day = soup.find(id="seven-day-forecast")
 forecast_items = seven_day.find_all(class_="tombstone-container")
 tonight = forecast_items[0]
 period_tags = seven_day.select(".tombstone-container .period-name")
-periods = [pt.get_text() for pt in period_tags]
-short_descs = [sd.get_text() for sd in seven_day.select(".tombstone-container .short-desc")]
-temps = [t.get_text() for t in seven_day.select(".tombstone-container .temp")]
-descs = [d["title"] for d in seven_day.select(".tombstone-container img")]
+periods = [str(pt.get_text()) for pt in period_tags]
+short_descs = [str(sd.get_text()) for sd in seven_day.select(".tombstone-container .short-desc")]
+temps = [str(t.get_text()) for t in seven_day.select(".tombstone-container .temp")]
+descs = [str(d["title"]) for d in seven_day.select(".tombstone-container img")]
 
 #current conditions
 current_conditions = soup.find(id="current-conditions")
 conditions = current_conditions.select("td")
-humidity = conditions[1]
-wind_speed = conditions[3]
-dewpoint = conditions[7]
+humidity = conditions[1].get_text()
+wind_speed = conditions[3].get_text()
+dewpoint = conditions[7].get_text()
 last_update = conditions[13].get_text()
 index = 0
 for char in range(len(last_update)):
@@ -41,21 +58,37 @@ for char in range(len(last_update)):
         break
 last_update = last_update[char:]
 
-curr = pd.DataFrame({
+curr = {
+    "zipcode": zipcode,
     "humidity": humidity,
     "wind_speed": wind_speed,
     "dewpoint": dewpoint,
     "last_update": last_update.strip()
-}, index=[0])
+}
 print("Current conditions: ")
 print(curr)
 
-weather = pd.DataFrame({
+weather = {
+"zipcode": zipcode,
  "period": periods,
  "short_desc": short_descs,
  "temp": temps,
  "desc":descs
-})
+}
 print("Seven day forecast:")
-print(weather)
+print(pd.DataFrame.from_dict(weather))
+
+
+db = client["weather_database"]
+current_conditions_db = db.current_conditions
+current_conditions_db_id = current_conditions_db.insert_one(curr).inserted_id
+
+seven_day_db = db.seven_day
+seven_day_db_id = seven_day_db.insert_one(weather).inserted_id
+print("Database info")
+print(client.list_database_names())
+print(db.list_collection_names())
+print(db.current_conditions_db)
+print(db.seven_day)
+
 
